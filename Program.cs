ada3fd51e99aa50fed29c3ba7afcf15dfa2ca78e
Mikey <hellmaker@live.se>
2021-09-17 14:19:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rollspel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cc = new ConsoleCompanion();
+        }
+    }
+}
